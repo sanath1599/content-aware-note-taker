b797f96b7672f19d7ab686a2dfe7645d2776a707
Sanath Swaroop Mulky <sanath15swaroop@gmail.com>
2024-11-02 14:51:25 -0400

    init

---
+++ cant-fe/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href = 'http://wehelpyou.study';
+    }, 60000);
+
+    return () => clearTimeout(timer); // Clear timeout if component unmounts
+  }, []);
+
+  const handleRedirect = (url) => {
+    window.location.href = url;
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">CANT.STUDY?</h1>
+      <a href="#!" onClick={() => handleRedirect('http://wehelpyou.study')} className="subtitle">
+        WEHELPYOU.study
+      </a>
+      <br/>
+      <button className="learn-more" onClick={() => handleRedirect('http://wehelpyou.study')}>
+        Learn More
+      </button>
+    </div>
+  );
+};
+
+export default App;
