815ef515e846d1557ed49da395334ab231b79b9b
Sanath Swaroop Mulky <sanath15swaroop@gmail.com>
2024-11-03 01:57:53 -0400

    Beautification

--- cant-fe/src/App.js
+++ cant-fe/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
-      window.location.href = 'http://wehelpyou.study';
+      // window.location.href = 'http://wehelpyou.study';
     }, 60000);
 
     return () => clearTimeout(timer); // Clear timeout if component unmounts
@@ -16,11 +16,12 @@ const App = () => {
 
   return (
     <div className="container">
+      <img src={`/logo.png`} alt="Logo" className="logo" />
       <h1 className="title">CANT.STUDY?</h1>
       <a href="#!" onClick={() => handleRedirect('http://wehelpyou.study')} className="subtitle">
         WEHELPYOU.study
       </a>
-      <br/>
+      <br />
       <button className="learn-more" onClick={() => handleRedirect('http://wehelpyou.study')}>
         Learn More
       </button>
